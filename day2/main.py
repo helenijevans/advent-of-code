91a27f19fc7533aef9c9ddfb6a24e79c6e47864d
Helen Evans <helenijevans@gmail.com>
2022-12-02 05:55:26 +0000

    day 2 done - cleaning

--- day2/main.py
+++ day2/main.py
@@ -1,45 +1,4 @@
 # PART 1
-# opponentEnc = {
-#     "A": "Rock",
-#     "B": "Paper",
-#     "C": "Scissors"
-# }
-#
-# playerEnc = {
-#     "X": "Rock",
-#     "Y": "Paper",
-#     "Z": "Scissors"
-# }
-#
-# winner = {
-#     "Rock": "Paper",
-#     "Scissors": "Rock",
-#     "Paper": "Scissors"
-# }
-#
-# points = {
-#     "Rock": 1,
-#     "Paper": 2,
-#     "Scissors": 3,
-#     "win": 6,
-#     "draw": 3
-# }
-#
-# with open("input.txt", 'r', encoding='utf-8') as file:
-#     score = 0
-#     for line in file:
-#         choices = line.split()
-#         opponent_choice = opponentEnc[choices[0]]
-#         player_choice = playerEnc[choices[1]]
-#         if opponent_choice == player_choice:
-#             score += points["draw"] + points[player_choice]
-#         elif winner[opponent_choice] == player_choice:
-#             score += points["win"] + points[player_choice]
-#         else:
-#             score += points[player_choice]
-# print(score)
-
-# PART 2
 opponentEnc = {
     "A": "Rock",
     "B": "Paper",
@@ -47,9 +6,9 @@ opponentEnc = {
 }
 
 playerEnc = {
-    "X": "Lose",
-    "Y": "Draw",
-    "Z": "Win"
+    "X": "Rock",
+    "Y": "Paper",
+    "Z": "Scissors"
 }
 
 winner = {
@@ -66,9 +25,29 @@ points = {
     "draw": 3
 }
 
+# with open("input.txt", 'r', encoding='utf-8') as file:
+#     score = 0
+#     for line in file:
+#         choices = line.split()
+#         opponent_choice = opponentEnc[choices[0]]
+#         player_choice = playerEnc[choices[1]]
+#         if opponent_choice == player_choice:
+#             score += points["draw"] + points[player_choice]
+#         elif winner[opponent_choice] == player_choice:
+#             score += points["win"] + points[player_choice]
+#         else:
+#             score += points[player_choice]
+# print(score)
+
+# PART 2
+playerEnc = {
+    "X": "Lose",
+    "Y": "Draw",
+    "Z": "Win"
+}
+
 with open("input.txt", 'r', encoding='utf-8') as file:
     score = 0
-
     for line in file:
         inputs = line.split()
         opponent_choice = opponentEnc[inputs[0]]
@@ -79,7 +58,6 @@ with open("input.txt", 'r', encoding='utf-8') as file:
             score += points["win"] + points[winner[opponent_choice]]
         else:
             for losing_move, winning_move in winner.items():
-                print(losing_move, winning_move)
                 if winning_move == opponent_choice:
                     score += points[losing_move]
                     break

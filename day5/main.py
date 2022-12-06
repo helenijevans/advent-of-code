bcbb778251d93c11b8db0f797db82f2d049c4796
Helen Evans <helenijevans@gmail.com>
2022-12-06 07:07:42 +0000

    day 6 done

--- day5/main.py
+++ day5/main.py
@@ -17,20 +17,20 @@ for line in reversed(list(open("input_graph.txt"))):
 """
 For Part 2 to work, the rest of the Part 1 lines need to be commented out (the above is the initial graph setup)
 """
-# with open("input.txt", 'r', encoding='utf-8') as file:
-#     for line in file:
-#         instructions = re.findall('\d+', line)
-#         no_of_crates = instructions[0]
-#         start_stack = instructions[1]
-#         end_stack = instructions[2]
-#         for i in range(0, int(no_of_crates)):
-#             move_crate = globals()['group%s' % start_stack].pop()
-#             globals()['group%s' % end_stack].append(move_crate)
-#
-# result = []
-# for i in range(1,10):
-#     result.append((globals()['group%s' % i].pop()))
-# print("".join(re.findall('[A-Z]', "".join(result))))
+with open("input.txt", 'r', encoding='utf-8') as file:
+    for line in file:
+        instructions = re.findall('\d+', line)
+        no_of_crates = instructions[0]
+        start_stack = instructions[1]
+        end_stack = instructions[2]
+        for i in range(0, int(no_of_crates)):
+            move_crate = globals()['group%s' % start_stack].pop()
+            globals()['group%s' % end_stack].append(move_crate)
+
+result = []
+for i in range(1,10):
+    result.append((globals()['group%s' % i].pop()))
+print("".join(re.findall('[A-Z]', "".join(result))))
 
 # PART 2
 with open("input.txt", 'r', encoding='utf-8') as file:
---
+++ day6/main.py
@@ -0,0 +1,23 @@
+# PART 1 & PART 2 Code
+from collections import deque
+
+marker = deque()
+
+
+def packet_processing(marker_length):
+    with open("input.txt", 'r', encoding='utf-8') as file:
+        marker_at_char = 0
+        while True:
+            char = file.read(1)
+            marker.append(char)
+            marker_at_char += 1
+            if len(marker) == marker_length:
+                if len(set(marker)) == marker_length:
+                    return marker_at_char
+                marker.popleft()
+
+
+# RUN FOR PART 1 ANSWER
+print(packet_processing(marker_length=4))
+# RUN FOR PART 2 ANSWER
+print(packet_processing(marker_length=14))
